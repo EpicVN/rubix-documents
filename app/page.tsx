6bcf1872281b810a8fb79b665627ac3d3e697bae
EpicVN <ledangkhoahi3@gmail.com>
2024-12-22 01:10:04 +0700

    First commit: Project setup

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,12 @@ import { PageRoutes } from "@/lib/pageroutes";
 export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
-      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-        Documents
-      </h1>
+      <h1 className="text-4xl font-bold mb-4 sm:text-7xl">LangChain</h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-        A simple open-source product documentation platform. That&apos;s simple to use and easy to customize.
+        Build context-aware, reasoning applications with LangChain’s flexible
+        framework that leverages your company’s data and APIs. Future-proof your
+        application by making vendor optionality part of your LLM infrastructure
+        design.
       </p>
       <div className="flex items-center gap-5">
         <Link
